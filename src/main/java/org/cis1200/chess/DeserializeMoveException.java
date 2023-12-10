19d45e28c831e1375b5c6a061c499fa15435e857
Alexander Robertson <xanrob@me.com>
2023-12-10 13:36:02 -0500

    Added message to `DeserializeMoveException`

--- src/main/java/org/cis1200/chess/DeserializeMoveException.java
+++ src/main/java/org/cis1200/chess/DeserializeMoveException.java
@@ -1,5 +1,13 @@
 package org.cis1200.chess;
 
 public class DeserializeMoveException extends Exception {
+    private String reason;
 
+    public DeserializeMoveException(String reason) {
+        this.reason = reason;
+    }
+
+    public String toString() {
+        return reason;
+    }
 }
